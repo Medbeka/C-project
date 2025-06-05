d130c22c89f5ff6c1851207031d80ce6767e2e7f
Mehdi Bakhta <themehdi121@gmail.com>
2025-06-05 20:49:07 +0100

    Update Program.cs to support AgentB

--- c/ConsoleApp2/Program.cs
+++ c/ConsoleApp2/Program.cs
@@ -1,19 +1,37 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Pipes;
 using System.Threading;
 
 class Program
 {
-    static void Main(string[] args)
+    static Dictionary<string, Dictionary<string, int>> combinedIndex = new();
+    static object lockObj = new();
+
+    static void Main()
     {
-        Console.WriteLine("Master started. Waiting for Agent A...");
+        Console.WriteLine("Master started. Waiting for Agent A and Agent B...");
 
         Thread agentAThread = new Thread(() => HandleAgentPipe("agentA"));
-        agentAThread.Start();
-        agentAThread.Join();
+        Thread agentBThread = new Thread(() => HandleAgentPipe("agentB"));
 
-        Console.WriteLine("Master finished.");
+        agentAThread.Start();
+        agentBThread.Start();
+
+        agentAThread.Join();
+        agentBThread.Join();
+
+        Console.WriteLine("\n--- Combined Index ---");
+        foreach (var word in combinedIndex.Keys)
+        {
+            foreach (var file in combinedIndex[word])
+            {
+                Console.WriteLine($"{file}:{word}:{combinedIndex[word][file]}");
+            }
+        }
+
+        Console.WriteLine("\nMaster finished.");
     }
 
     static void HandleAgentPipe(string pipeName)
@@ -27,10 +45,26 @@ class Program
             string? line;
             while ((line = reader.ReadLine()) != null)
             {
-                Console.WriteLine($"Received: {line}");
+                string[] parts = line.Split(':');
+                if (parts.Length != 3) continue;
+
+                string file = parts[0];
+                string word = parts[1];
+                int count = int.Parse(parts[2]);
+
+                lock (lockObj)
+                {
+                    if (!combinedIndex.ContainsKey(word))
+                        combinedIndex[word] = new Dictionary<string, int>();
+
+                    if (!combinedIndex[word].ContainsKey(file))
+                        combinedIndex[word][file] = 0;
+
+                    combinedIndex[word][file] += count;
+                }
             }
 
-            Console.WriteLine($"Finished receiving data from {pipeName}.");
+            Console.WriteLine($"Finished receiving from {pipeName}");
         }
         catch (Exception ex)
         {
@@ -38,3 +72,4 @@ class Program
         }
     }
 }
+
