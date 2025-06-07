a47b98f576f85d970883dcde6fd5160ae0e32d7e
Mehdi Bakhta <themehdi121@gmail.com>
2025-06-07 21:44:28 +0100

    Update and rename Program.cs to master.cs

--- c/ConsoleApp2/master.cs
+++ c/ConsoleApp2/master.cs
@@ -3,18 +3,31 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Pipes;
 using System.Threading;
+using System.Diagnostics;
+using System.Collections.Concurrent;
 
 class Program
 {
-    static Dictionary<string, Dictionary<string, int>> combinedIndex = new();
-    static object lockObj = new();
+    static ConcurrentDictionary<string, Dictionary<string, int>> combinedIndex = new();
+    static readonly object lockObj = new();
 
-    static void Main()
+    static void Main(string[] args)
     {
+        SetCpuAffinity(2); // Master on CPU core 2
+
         Console.WriteLine("Master started. Waiting for Agent A and Agent B...");
 
-        Thread agentAThread = new Thread(() => HandleAgentPipe("agentA"));
-        Thread agentBThread = new Thread(() => HandleAgentPipe("agentB"));
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Please provide two pipe names as arguments.");
+            return;
+        }
+
+        string pipe1 = args[0];
+        string pipe2 = args[1];
+
+        Thread agentAThread = new Thread(() => HandleAgentPipe(pipe1));
+        Thread agentBThread = new Thread(() => HandleAgentPipe(pipe2));
 
         agentAThread.Start();
         agentBThread.Start();
@@ -27,11 +40,19 @@ class Program
         {
             foreach (var file in combinedIndex[word])
             {
-                Console.WriteLine($"{file}:{word}:{combinedIndex[word][file]}");
+                Console.WriteLine($"{file.Key}:{word}:{file.Value}");
             }
         }
 
         Console.WriteLine("\nMaster finished.");
+    }
+
+    static void SetCpuAffinity(int cpuIndex)
+    {
+        var process = Process.GetCurrentProcess();
+        IntPtr mask = (IntPtr)(1 << cpuIndex);
+        process.ProcessorAffinity = mask;
+        Console.WriteLine($"Master running on CPU core {cpuIndex}");
     }
 
     static void HandleAgentPipe(string pipeName)
@@ -50,7 +71,7 @@ class Program
 
                 string file = parts[0];
                 string word = parts[1];
-                int count = int.Parse(parts[2]);
+                if (!int.TryParse(parts[2], out int count)) continue;
 
                 lock (lockObj)
                 {
@@ -72,4 +93,3 @@ class Program
         }
     }
 }
-
